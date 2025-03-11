5671cdd84c2136d4ad4265fc9c6a26910ed9315e
ryane-jon <r.jones10@rgu.ac.uk>
2025-03-11 10:39:00 +0000

    continued constructors.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 //Game Constructor
-function Game(title, console, developer, publisher, series, availability, consoleAvailability, id) {
+function Game(title, console, developer, publisher, series, availability, reviews, id) {
     this.title = title; // The title of the game -- String
     this.console = console; // The console(s) that the game is on -- Array of String
     this.developer = developer; // The Developer of the game -- String
     this.publisher = publisher; // The Publisher of the game -- String
     this.series = series; // The series the game is a part of (if any) -- String
     this.availability = availability; // is the game still available to buy -- String -- "Available", "Overpriced", "Partial", "Unavailable" etc
-    
+    this.reviews = reviews // the existing reviews for this game -- Array of ReviewScore
     this.id = id; // self explanitory -- int
 }
 
@@ -23,4 +23,11 @@ function User(username, password, email, subsctiption) {
     password = this.password; // String
     email = this.email; // String
     subsctiption = this.subsctiption; // boolean
+}
+
+//Review Constructor
+function ReviewScore(score, game, source) {
+    score = this.score; // int
+    game = this.game; // the game the review is for -- String
+    source = this.source; // The source of the review -- String -- "metacritic", "IGN" etc
 }
